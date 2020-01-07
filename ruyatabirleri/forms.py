b45df2dcb39d1eac25365508b5df7127c69c041e
ruyatabirix <ruyatabirix@outlook.com>
2020-01-07 23:41:13 +0300

    geliştirme

--- ruyatabirleri/forms.py
+++ ruyatabirleri/forms.py
@@ -10,22 +10,5 @@ class AramaForm (forms.ModelForm):
 
         labels = { 'kelime': '',}
         error_messages = {'kelime': {'max_length': "This is too long",},}
-        widgets = { 'kelime': forms.TextInput (attrs={'style': 'width: 400px; font-size: 20px; padding: 15px 10px;',
+        widgets = { 'kelime': forms.TextInput (attrs={'style': 'border-style: none; width: 400px; font-size: 20px; padding: 15px 10px;',
                                                     'placeholder': 'Aramak İstediğiniz Kelimeyi Yazınız.',}),}
-
-
-"""
-class CommentForm(forms.ModelForm):
-
-    class Meta:
-        model = Comment
-        fields = ('author', 'text',)
-
-        labels = { 'author': '', 'text': '',}
-        error_messages = {'author': {'max_length': "This is too long",},}
-        widgets = { 'author': forms.TextInput (attrs={'style': 'width: 500px; font-size: 20px;',
-                                                    'placeholder': 'Yazar',}),
-                    'text': forms.Textarea (attrs={'style': 'width: 500px; font-size: 20px;',
-                                                      'placeholder': 'Yorum Yaz', }), }
-
-"""
