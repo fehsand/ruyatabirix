038c5f41fc7c54424bd8b78b6d7740e8f9f743a6
ruyatabirix <ruyatabirix@outlook.com>
2020-01-08 02:15:31 +0300

    geliştirme5

--- ruyatabirleri/forms.py
+++ ruyatabirleri/forms.py
@@ -7,8 +7,3 @@ class AramaForm (forms.ModelForm):
     class Meta:
         model = ArananKelimeler
         fields = ('kelime',)
-
-        labels = { 'kelime': '',}
-        error_messages = {'kelime': {'max_length': "This is too long",},}
-        widgets = { 'kelime': forms.TextInput (attrs={'style': 'border-style: none; font-size: 20px; padding: 15px 10px;',
-                                                    'placeholder': 'Aramak İstediğiniz Kelimeyi Yazınız.',}),}
