608179c10a978b03b4c362246f6070853691be82
ruyatabirix <ruyatabirix@outlook.com>
2020-01-13 01:55:31 +0300

    geliştirme

--- ruyatabirix/settings.py
+++ ruyatabirix/settings.py
@@ -70,8 +70,12 @@ WSGI_APPLICATION = 'ruyatabirix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ruyatabirix',
+        'USER': 'root',
+        'PASSWORD': 'Tgbyhn123+',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
