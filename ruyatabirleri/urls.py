44daae3f1210de73a43be360676547329e88973b
fehsand <fehsand@yahoo.com>
2020-02-07 22:33:23 +0300

    Gelistirme.

--- ruyatabirleri/urls.py
+++ ruyatabirleri/urls.py
@@ -5,11 +5,11 @@ app_name = "ruyatabirleri"
 
 urlpatterns = [
     path('', views.ruyatabirleri, name='ruyatabirleri'),
-    path('ruyatabirleri/ruyada-<slug:slug>-gormek/', views.ruyatabirleri_ayrinti, name='ruyatabirleri_ayrinti'),
-    path('ruyatabirleri/ruyada-<slug:slug>-gormek/', views.ruyatabirleri_ayrinti, name='my_comment_was_posted'),
+    path('ruyatabirleri/ruyada-<slug:slug>-gormek/q1w2e3r4t5y6u7', views.ruyatabirleri_ayrinti, name='ruyatabirleri_ayrinti'),
+    path('ruyatabirleri/ruyada-<slug:slug>-gormek/q1w2e3r4t5y6u7', views.ruyatabirleri_ayrinti, name='my_comment_was_posted'),
     path('ruyatabirleri_ruya_nedir/', views.ruya_nedir, name='ruya_nedir'),
     path('ruyatabirleri_ruya_ile_amel/', views.ruya_ile_amel, name='ruya_ile_amel'),
     path('ruyatabirleri_ruya_hadis/', views.ruya_hadis, name='ruya_hadis'),
     path('ruyatabirleri_gizlilik/', views.gizlilik, name='gizlilik'),
-    path('<str:harf>/', views.harf_sayfalari, name='harf_sayfalari'),
+    path('<str:harf>/q1w2e3r4t5y6u7', views.harf_sayfalari, name='harf_sayfalari'),
 ]
