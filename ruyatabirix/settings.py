a5b237fbb640a81d742878c01687117cc05b9c86
fehsand <fehsand@yahoo.com>
2020-06-21 17:17:07 +0300

    Gelistirme.

--- ruyatabirix/settings.py
+++ ruyatabirix/settings.py
@@ -4,20 +4,25 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+#------------------------------------------------------------------------------
+# local de açık yayında kapalı
 SECRET_KEY = 'kg1!kr3a4h_*_79*p=p&_lht6&-u111u6uo=79g*8dqv3@ej!*'
+# local de kapalı yayında açık olacak
+#with open('/home/ruyatabirix/sc_ky.txt') as f:
+#    SECRET_KEY = f.read().strip()
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
+#--------------------------------------------------------------------------------
+# local de açık yayında kapalı
 DEBUG = True
+# local de kapalı yayında açık olacak
+#DEBUG = False
+#--------------------------------------------------------------------------------
+
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.ruyatabirix.com',]
 
-
-# Application definition
-
+#--------------------------------------------------------------------------------
 INSTALLED_APPS = [
     'ruyatabirleri.apps.RuyatabirleriConfig',
     'django.contrib.admin',
@@ -31,8 +36,14 @@ INSTALLED_APPS = [
     'register',
     'django.contrib.sitemaps',
 ]
+#-----------------------------------------------------------
 
+#localde açık yayında kapalı
 SITE_ID = 1
+#yayında açık localde kapalı
+#SITE_ID = 2
+
+#---------------------------------------------------
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -43,10 +54,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+#----------------------------------------------------------
 
+# local de açık yayında kapalı
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
+
+# yayında açık localde kapalı
 #SESSION_COOKIE_SECURE = True
 #CSRF_COOKIE_SECURE = True
 
@@ -54,6 +69,8 @@ X_FRAME_OPTIONS = 'DENY'
 #SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 #SECURE_HSTS_PRELOAD = True
 #SECURE_SSL_REDIRECT = True
+
+#----------------------------------------------------------------
 
 ROOT_URLCONF = 'ruyatabirix.urls'
 
@@ -75,11 +92,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ruyatabirix.wsgi.application'
 
+#-----------------------------------------------------------------
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-
+#local de açık yayında kapalı
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -91,6 +106,19 @@ DATABASES = {
     }
 }
 
+#yayında açık local de kapalı
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'ruyatabirix$ruyatabirix',
+#        'USER': 'ruyatabirix',
+#        'PASSWORD': 'Rfvtgb123+',
+#        'HOST': 'ruyatabirix.mysql.pythonanywhere-services.com',
+#    }
+#}
+
+#--------------------------------------------------------------------------
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -131,4 +159,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
