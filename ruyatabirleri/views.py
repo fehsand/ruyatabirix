8edb687770d8953328c7454aa036b035c49e2817
fehsand <fehsand@yahoo.com>
2020-07-12 00:13:55 +0300

    Gelistirme.

--- ruyatabirleri/views.py
+++ ruyatabirleri/views.py
@@ -1,6 +1,6 @@
 import random
 from .forms import AramaForm, YildiznameForm, iletisimForm
-from .models import Ruyatabirleri, KuranBilgi, KuranKelime, ArananKelimeler, Ruyatabirlerix, Ruyatabirlerix_sbt
+from .models import Ruyatabirleri, KuranBilgi, KuranKelime, ArananKelimeler, Ruyatabirlerix, Ruyatabirlerix_sbt, Ruyatabirlerix3
 from django.shortcuts import render, get_object_or_404
 from django.utils.translation import get_language, gettext_lazy as _
 
